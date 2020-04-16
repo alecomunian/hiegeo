a4e7bda2946553bc3d5069b58a6f2462586c2855
alecomunian <alessandro.comunian@unimi.it>
2020-04-16 23:27:02 +0200

    Improved the configuration of the installation files.

--- setup.py
+++ setup.py
@@ -5,24 +5,37 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="hiegeo",
-    version="0.1.0",
+    version="0.1.5",
     author="Alessandro Comunian",
-    author_email="alessandro DOC comunian AT unimi DOI it",
+    author_email="alessandro.comunian@unimi.it",
     description="Modelling stratigraphic alluvial architectures, constrained by stratigraphic hierarchy and relative chronology",
     long_description=long_description,
     long_description_content_type="text/x-rst",
     url="https://bitbucket.org/alecomunian/hiegeo",
-    packages=setuptools.find_packages(where="hiegeo"),
+    packages=['hiegeo'],
+    py_modules=["hiegeo"],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU GPL License",
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         "Operating System :: OS Independent",
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Development Status :: 3 - Alpha',
+        'Topic :: Scientific/Engineering'
     ],
+    keywords = "geology, modelling, hierarchy, chronology",
+    project_urls = {
+        'Documentation': 'https://hiegeo.readthedocs.io/en/latest/index.html',
+        'Source': 'https://bitbucket.org/alecomunian/hiegeo',
+        },
+    
     python_requires='>=3.6',
     install_requires=[
-        "numpy>=1.18.1",
+        "numpy",
         "matplotlib",
         "anytree",
         "pandas"],
-    package_dir = {"": "hiegeo"},
+    package_dir = {"": "."},
 )
+
+
